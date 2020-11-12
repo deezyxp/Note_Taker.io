19241044c0e925e7260c95578b019eb45031982b
deezy <danielshiaolin@gmail.com>
2020-11-12 15:25:35 +1100

    final touches to code

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require("path")
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware that functions with Express and Node 
+
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
-//Routes below passing in express to routes files
+
 require("./routes/htmlroutes.js")(app);
 require("./routes/apiroutes.js")(app);
 
